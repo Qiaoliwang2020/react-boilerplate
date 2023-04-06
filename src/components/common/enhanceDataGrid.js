a7257d86ad9c5c00494f2efa8fa7f3b46e7b5c9f
Qiaoliwang2020 <414217795zoe@gmail.com>
2023-04-06 16:22:47 +1000

    improve data grid component

--- src/components/common/enhanceDataGrid.js
+++ src/components/common/enhanceDataGrid.js
@@ -1,80 +1,49 @@
 import { Typography } from '@mui/material';
 import Box from '@mui/material/Box';
 import { DataGrid,GridToolbar } from '@mui/x-data-grid';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
+import PropTypes from 'prop-types';
 
-const columns = [
-  { field: 'id', headerName: 'ID', width: 90 },
-  {
-    field: 'firstName',
-    headerName: 'First name',
-    width: 150,
-    editable: true,
-  },
-  {
-    field: 'lastName',
-    headerName: 'Last name',
-    width: 150,
-    editable: true,
-  },
-  {
-    field: 'age',
-    headerName: 'Age',
-    type: 'number',
-    width: 110,
-    editable: true,
-  },
-  {
-    field: 'fullName',
-    headerName: 'Full name',
-    description: 'This column has a value getter and is not sortable.',
-    sortable: true,
-    width: 160,
-    valueGetter: (params) =>
-      `${params.row.firstName || ''} ${params.row.lastName || ''}`,
-  },
-];
 
-const rows = [
-  { id: 1, lastName: 'Snow', firstName: 'Jon', age: 35 },
-  { id: 2, lastName: 'Lannister', firstName: 'Cersei', age: 42 },
-  { id: 3, lastName: 'Lannister', firstName: 'Jaime', age: 45 },
-  { id: 4, lastName: 'Stark', firstName: 'Arya', age: 16 },
-  { id: 5, lastName: 'Targaryen', firstName: 'Daenerys', age: null },
-  { id: 6, lastName: 'Melisandre', firstName: null, age: 150 },
-  { id: 7, lastName: 'Clifford', firstName: 'Ferrara', age: 44 },
-  { id: 8, lastName: 'Frances', firstName: 'Rossini', age: 36 },
-  { id: 9, lastName: 'Roxie', firstName: 'Harvey', age: 65 },
-];
-
-export default function EnhancedDataGrid() {
+export default function EnhancedDataGrid(props) {
+  const [columns, setColumns] = useState([]);
   useEffect(()=>{
-    let columns =Object.keys(rows[0])?.map((key)=>{
+    let columns =Object.keys(props.dataRow[0])?.map((key)=>{
       return {
         field: key,
-        headerName: key,
+        headerName: key.replaceAll("_", ""),
         description: '',
         sortable: true,
         width: 160,
       };
     });
-    console.log(columns);
-  },[]);
+    setColumns(columns);
+  },[props.dataRow]);
 
   return (
     <Box sx={{ height: 615, width: '100%' }}>
-      <Typography variant='h5' className='table-title'>title here</Typography>  
+      <Typography variant='h5' className='table-title'>{props?.title ?? ''}</Typography>  
       <DataGrid
-        loading={false}
-        isCellEditable={() => false}
-        components={{ Toolbar: GridToolbar }}
-        rows={rows}
+        loading={props?.loading ?? false}
+        isCellEditable={() =>props?.isCellEditable ?? false}
+        components={{ Toolbar: props?.isToolBarOn ? GridToolbar : '' }}
+        getRowId={(row) => row?._id}
+        rows={props?.dataRow}
         columns={columns}
-        pageSize={5}
-        rowsPerPageOptions={[5]}
+        pageSize={props?.pageSize ?? 5}
+        rowsPerPageOptions={props?.rowsPerPageOptions ?? [5]}
         disableSelectionOnClick
         experimentalFeatures={{ newEditingApi: true }}
       />
     </Box>
   );
 }
+EnhancedDataGrid.propTypes ={
+  title:PropTypes.string,
+  loading:PropTypes.bool,
+  isCellEditable:PropTypes.bool,
+  isToolBarOn:PropTypes.bool,
+  dataRow:PropTypes.array.isRequired,
+  pageSize:PropTypes.number,
+  rowsPerPageOptions:PropTypes.array
+};
--- src/views/example/index.js
+++ src/views/example/index.js
@@ -3,12 +3,23 @@ import { Box, Container } from '@mui/material';
 import { LayoutConfig } from '../../constants';
 import { EnhancedDataGrid } from '../../components';
 
+const rows = [
+  { _id: 1, lastName: 'Snow', firstName: 'Jon', age: 35 },
+  { _id: 2, lastName: 'Lannister', firstName: 'Cersei', age: 42 },
+  { _id: 3, lastName: 'Lannister', firstName: 'Jaime', age: 45 },
+  { _id: 4, lastName: 'Stark', firstName: 'Arya', age: 16 },
+  { _id: 5, lastName: 'Targaryen', firstName: 'Daenerys', age: null },
+  { _id: 6, lastName: 'Melisandre', firstName: null, age: 150 },
+  { _id: 7, lastName: 'Clifford', firstName: 'Ferrara', age: 44 },
+  { _id: 8, lastName: 'Frances', firstName: 'Rossini', age: 36 },
+  { _id: 9, lastName: 'Roxie', firstName: 'Harvey', age: 65 },
+];
 
 export const Example = () => {
   return (<Box sx={LayoutConfig.defaultContainerSX}>
     <Container
       maxWidth="xl">
-      <EnhancedDataGrid></EnhancedDataGrid>
+      <EnhancedDataGrid title={'Example'} isToolBarOn={false} dataRow={rows}></EnhancedDataGrid>
     </Container>
   </Box>);
 };
